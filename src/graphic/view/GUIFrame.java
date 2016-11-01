87e276f63266b28d6d60fad1441a87879386453a
mattpow35 <mpow2231@740s-pl-2.local>
2016-11-01 08:35:55 -0600

    added the panel to the frame

--- src/graphic/view/GUIFrame.java
+++ src/graphic/view/GUIFrame.java
@@ -7,17 +7,21 @@ import java.awt.Dimension;
 public class GUIFrame extends JFrame
 {
 	private GUIFrameworkController baseController;
+	private GUIPanel appPanel;
 	
 	public GUIFrame(GUIFrameworkController baseController)
 	{
 		super();
 		this.baseController = baseController;
+		appPanel = new GUIPanel(baseController);
 		
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
+		this.setContentPane(appPanel);
+		this.setTitle("WOW a window");
 		this.setSize(new Dimension(500, 500));
 		this.setVisible(true);
 	}
