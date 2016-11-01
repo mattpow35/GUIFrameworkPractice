25ff2f8eb2dd0390c8d08e4f0214b8bfc007340a
mattpow35 <mpow2231@740s-pl-2.local>
2016-11-01 07:54:18 -0600

    created first GUI project

---
+++ src/graphic/controller/GUIFrameworkRunner.java
@@ -0,0 +1,11 @@
+package graphic.controller;
+
+public class GUIFrameworkRunner
+{
+	public static void main(String [] args)
+	{
+		GUIFrameworkController GUIAppController = new GUIFrameworkController();
+		GUIAppController.start();
+	}
+
+}
