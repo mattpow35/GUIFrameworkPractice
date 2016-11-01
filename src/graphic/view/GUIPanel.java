ae74243a5eddc7c08d80d85718c8019b5667f88a
mattpow35 <mpow2231@740s-pl-2.local>
2016-11-01 09:07:17 -0600

    added buttons and linked it with the panel with a listener

--- src/graphic/view/GUIPanel.java
+++ src/graphic/view/GUIPanel.java
@@ -5,40 +5,49 @@ import javax.swing.JButton;
 import graphic.controller.GUIFrameworkController;
 import java.awt.Color;
 
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class GUIPanel extends JPanel
 {
 	private GUIFrameworkController baseController;
 	private JButton colorButton;
 	private JButton randomButton;
-	
+
 	public GUIPanel(GUIFrameworkController baseController)
 	{
 		super();
 		this.baseController = baseController;
-		
+
 		colorButton = new JButton("Color!");
 		randomButton = new JButton("Random");
-		
+
 		setupPanel();
 		setupLayout();
-		setupListener();
+		setupListeners();
 	}
-	
+
 	private void setupPanel()
 	{
 		this.setBackground(Color.CYAN);
 		this.add(colorButton);
 		this.add(randomButton);
 	}
-	
+
 	private void setupLayout()
 	{
-		
+
 	}
-	
-	private void setupListener()
+
+	private void setupListeners()
 	{
-		
+		colorButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				System.out.println("Hey you clicked this!");
+			}
+		});
 	}
 
 }
